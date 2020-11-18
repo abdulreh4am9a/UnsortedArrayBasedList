6b993bae242f04aa54a6bbcdd1e3b0fb0591306b
abdulreh4am9a <abdulreh4am9a@gmail.com>
2020-11-18 12:16:04 +0500

    Add project files.

---
+++ UnsortedArrayBasedList/list.h
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class list {
+	T* data;
+	int size, n;
+	int find_idx(const T& val) const {
+		for (int i = 0;i <= n;i++) {
+			if (val == data[i])
+				return i;
+		}
+		return -1;
+	}
+public:
+	list(int s = 100) {
+		size = s;
+		data = new T[size];
+		n = -1;
+	}
+	~list() {
+		delete[]data;
+	}
+	void insert(const T& val) {
+		if (full()) {
+			throw("List is full");
+		}
+		n++;
+		data[n] = val;
+	}
+	bool find(const T& val) const {
+		return (find_idx(val) >= 0);
+	}
+	bool update(const T& old_val, const T& new_val) {
+		int idx = find_idx(old_val);
+		if (idx >= 0) {
+			data[idx] = new_val;
+			return true;
+		}
+		return false;
+	}
+	bool erase(const T& val) {
+		int idx = find_idx(val);
+		if (idx >= 0) {
+			data[idx] = data[n];
+			n--;
+			return true;
+		}
+		return false;
+	}
+	bool empty() const {
+		return n == -1;
+	}
+	bool full() const {
+		return n == size - 1;
+	}
+	void make_empty() {
+		n = -1;
+	}
+	int length()const {
+		return n + 1;
+	}
+};
